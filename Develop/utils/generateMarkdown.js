077b90fe52c5ade917d12e74dbf0f378ac94b8d8
kyndalbowers <131105491+kyndalbowers@users.noreply.github.com>
2023-08-03 19:22:50 -0700

    generateMarkdown code

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,20 +1,87 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// Function that returns a license badge based on which license is passed in
+// If there is no license, returns an empty string
+function renderLicenseBadge(license) {
+  if (license === 'AGPLv3') {
+    return '![License: AGPL v3](https://img.shields.io/badge/License-AGPLv3-blue.svg)';
+  } else if (license === 'Apache 2.0') {
+    return '![License: Apache 2.0](https://img.shields.io/badge/License-Apache%202.0-blue.svg)';
+  } else {
+    return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+// Function that returns the license link
+// If there is no license, returns an empty string
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'GNU AGPLv3':
+      return '[GNU Affero General Public License v3.0](https://www.gnu.org/licenses/agpl-3.0)';
+    case 'GNU GPLv3':
+      return '[GNU General Public License v3.0](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'Mozilla Public License 2.0':
+      return '[Mozilla Public License 2.0](https://www.mozilla.org/en-US/MPL/2.0/)';
+    case 'Apache License 2.0':
+      return '[Apache License 2.0](https://www.apache.org/licenses/LICENSE-2.0)';
+    case 'MIT License':
+      return '[MIT License](https://opensource.org/licenses/MIT)';
+    case 'Boost Software License 1.0':
+      return '[Boost Software License 1.0](https://www.boost.org/LICENSE_1_0.txt)';
+    case 'The Unlicense':
+      return '[The Unlicense](http://unlicense.org/)';
+    default:
+      return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+// Function that returns the license section of README
+// If there is no license, returns an empty string
+function renderLicenseSection(license) {
+  if (license) {
+    const licenseBadge = renderLicenseBadge(license);
+    const licenseLink = renderLicenseLink(license);
+    return `## License
+${licenseBadge}
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the ${licenseLink} license.`;
+  } else {
+    return '';
+  }
+}
+
+// Function to generate markdown for README
 function generateMarkdown(data) {
+  const licenseSection = renderLicenseSection(data.license);
+
   return `# ${data.title}
 
-`;
+## Description
+${data.description}
+
+## Table of Contents
+${data["table of contents"]}
+
+## Installation
+${data.installation}
+
+## Usage
+${data.usage}
+
+## Credits
+${data.credits}
+
+${licenseSection}
+
+## Badges
+${data.badges}
+
+## Features
+${data.features}
+
+## How to Contribute
+${data.contribute}
+
+## Tests
+${data.tests}`;
 }
 
 module.exports = generateMarkdown;
